67ae3e29ad99f6d4b2ae656f530467ddb8c23638
Kateryna Lutska <ket.lutska@gmail.com>
2021-03-04 17:43:00 +0200

    add base markup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+// бібліотеки json //
+
+// import
+import Container from './components/container/Container';
+
+import s from './App.css';
+
+function App() {
+  return (
+    <div className={s.App}>
+      <Container></Container>
+    </div>
+  );
+}
+
+export default App;
